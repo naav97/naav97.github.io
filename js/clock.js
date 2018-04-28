5ce6632826f2bcc3353f4417b27a9ef26acf59ae
Nicolas Alvarado <na.alvarado@uniandes.edu.co>
2018-04-27 22:21:34 -0500

    add margin

--- js/clock.js
+++ js/clock.js
@@ -1,22 +1,12 @@
 function startTime()
 {
-   var today=new Date();
-   var h=today.getHours();
-   var m=today.getMinutes();
-   var s=today.getSeconds();
-   var ts = Math.round((new Date()).getTime() / 1000);
+   var today = new Date();
    var d = today.getDate();
    var mo = today.getMonth() + 1;
-   var y = today.getFullYear();
-   var d_names = ["Sunday","Monday", "Tuesday", "Wednesday",
-"Thursday", "Friday", "Saturday"];
+   var d_names = ["Sunday","Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"];
    var curr_day  = today.getDay();
-   // add a zero in front of numbers<10
-   m=checkTime(m);
-   s=checkTime(s);
    document.getElementById('dia').innerHTML=d_names[curr_day];
-   document.getElementById('horaFecha').innerHTML=h+":"+m+":"+s+" - "+d+"/"+mo+"/"+y;
-   t=setTimeout('startTime()',500);
+   document.getElementById('horaFecha').innerHTML = d+"/"+mo;
 }
 function checkTime(i)
 {
