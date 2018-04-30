9812aae90dc67e5fb4b42fc391024c3fed15c43a
Nicolas Alvarado <naav972@gmail.com>
2018-04-30 17:56:45 -0500

    cambio mes de numero a palabra

--- js/clock.js
+++ js/clock.js
@@ -3,10 +3,11 @@ function startTime()
    var today = new Date();
    var d = today.getDate();
    var mo = today.getMonth() + 1;
-   var d_names = ["Sunday","Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"];
    var curr_day  = today.getDay();
+   var d_names = ["Sunday","Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"];
+   var mo_names = ["January","February","March","April","May","June","July","August","September","October","November","December"];
    document.getElementById('dia').innerHTML=d_names[curr_day];
-   document.getElementById('horaFecha').innerHTML = d+"/"+mo;
+   document.getElementById('horaFecha').innerHTML = d+" of "+mo_names[mo];
 }
 function checkTime(i)
 {
