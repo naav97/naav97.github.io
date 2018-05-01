2efc667663aaef1eaa0a55259f13419a17c02371
Nicolas Alvarado <naav972@gmail.com>
2018-04-30 23:22:33 -0500

    background change

--- js/clock.js
+++ js/clock.js
@@ -21,5 +21,7 @@ function main( )
 {
   $(".todo").hide( );
   startTime();
+  var images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg', 'image6.jpg'];
+  $('html').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
   $(".todo").fadeIn(500);
 }
