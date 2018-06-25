14bff41d6a447ea311f8c5dd12ec80f76f5bc403
Nicolas <naav972@gmail.com>
2018-06-25 13:24:17 -0500

    Date updates

--- js/clock.js
+++ js/clock.js
@@ -8,6 +8,7 @@ function startTime()
    var mo_names = ["January","February","March","April","May","June","July","August","September","October","November","December"];
    document.getElementById('dia').innerHTML=d_names[curr_day];
    document.getElementById('horaFecha').innerHTML = mo_names[mo-1]+" "+d;
+   setTimeout(startTime(), 86400000);
 }
 function checkTime(i)
 {
