2a47246dfced367dd256a6da25803b244daeb47a
Nicolas <naav972@gmail.com>
2018-07-19 22:37:47 -0500

    more papes

--- js/clock.js
+++ js/clock.js
@@ -8,13 +8,13 @@ function startTime()
    var mo_names = ["January","February","March","April","May","June","July","August","September","October","November","December"];
    document.getElementById('dia').innerHTML=d_names[curr_day];
    document.getElementById('horaFecha').innerHTML = mo_names[mo-1]+" "+d;
-   setTimeout(startTime, 1000);
+   setTimeout(startTime, 10000000);
 }
 function main( )
 {
   $(".todo").hide( );
   startTime();
-  var images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg'];
+  var images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg', 'image9.jpg'];
   $('html').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
   $(".todo").fadeIn(500);
 }
