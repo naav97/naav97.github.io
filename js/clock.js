e1921e9558ddb86c2399245b0fa3b8d0f9b6cff1
Nicolas <naav97@localhost.localdomain>
2019-03-02 13:37:37 -0500

    nuevo pape

--- js/clock.js
+++ js/clock.js
@@ -14,7 +14,7 @@ function main( )
 {
   $(".todo").hide( );
   startTime();
-  var images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg', 'image9.jpg'];
+  var images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg', 'image9.jpg', 'image10.jpg'];
   $('html').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
   $(".todo").fadeIn(500);
 }
