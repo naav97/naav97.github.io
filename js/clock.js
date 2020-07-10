2b9117455280e39d0601562bd303f9f2857d0ac4
na.alvarado <na.alvarado@uniandes.edu.co>
2020-07-10 18:16:28 -0500

    new papes

--- js/clock.js
+++ js/clock.js
@@ -15,7 +15,22 @@ function startTime()
 }
 function main( )
 {
-    var images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg', 'image9.jpg', 'image10.jpg', 'image11.jpg', 'image12.jpg'];
+    var images = ['image1.jpg',
+                  'image2.jpg',
+                  'image3.jpg',
+                  'image4.jpg',
+                  'image5.jpg',
+                  'image6.jpg',
+                  'image7.jpg',
+                  'image8.jpg',
+                  'image9.jpg',
+                  'image10.jpg',
+                  'image11.jpg',
+                  'image12.jpg',
+                  'image13.jpg',
+                  'image14.jpg',
+                  'image15.jpg'
+                 ];
     var url = 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')';
     var alll = document.getElementsByTagName("HTML")[0];
     alll.style.backgroundImage = url;
