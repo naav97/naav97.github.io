618524c574bda909bededdd635573866810fe85c
Nicolas Alvarado <naav972@gmail.com>
2022-02-20 21:34:04 -0500

    js license

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
 function startTime()
 {
     var oneday = 24*60*60*100; 
@@ -138,3 +139,4 @@ function fileT(){
 function top1(){
     document.getElementById("top1").scrollIntoView({behavior: "smooth", block: "center"});
 }
+// @license-end
