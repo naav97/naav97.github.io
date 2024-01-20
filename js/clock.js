cab00316d14c0d5c7bc7648e1255e6bde2f9b9d0
Nicolas Alvarado <naav972@gmail.com>
2024-01-20 12:35:41 -0500

    added music stores, removed bloat, and added a keyboard shortcut

--- js/clock.js
+++ js/clock.js
@@ -19,72 +19,7 @@ function startTime()
 const main = () => {
     const url = 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')';
     document.getElementsByTagName("HTML")[0].style.backgroundImage = url;
-    startTime();
-}
-
-function getCorona(){
-    document.getElementById("loader").style.display = "block";
-    document.getElementById("respuestaC").style.display = "none";
-    var poblas = JSON.parse(paises);
-    var t = new Date();
-    t.setDate(t.getDate() - 1);
-    var ano = t.getFullYear();
-    var mes = t.getMonth() + 1;
-    var dia = t.getDate();
-    if ((mes+"").length < 2) {
-        mes = '0' + mes;
-    }
-    if ((dia+"").length < 2) {
-        dia = '0' + dia;
-    }
-    var fe = ano + "-" + mes + "-" + dia;
-    var pais = document.getElementById("cont").value;
-    if (pais === "uk") {
-        pais = "united kingdom";
-    }
-    var dato = new XMLHttpRequest();
-    dato.open("GET", "https://covid-api.com/api/reports?date="+fe+"&region_name="+pais);
-    dato.responseType = 'json';
-    dato.onload = function() {
-        let poblacion = 0;
-        var p;
-        for (p in poblas) {
-            var soni = poblas[p].Name.toUpperCase() === pais.toUpperCase();
-            if (soni) {
-                poblacion = poblas[p].Population*1000000;
-                break;
-            }
-        }
-        let confirmados = 0;
-        let activos = 0;
-        let recuperados = 0;
-        let muertos = 0;
-        var arreglo = dato.response.data;
-        var provincia;
-        for (provincia in arreglo) {
-            confirmados += arreglo[provincia].confirmed;
-            activos += arreglo[provincia].active;
-            recuperados += arreglo[provincia].recovered;
-            muertos  += arreglo[provincia].deaths;
-        }
-        let porconf = (confirmados * 100)/poblacion;
-        let poract = (activos *  100)/confirmados;
-        let porrec = (recuperados * 100)/(confirmados - activos);
-        let pormu = (muertos * 100)/(confirmados - activos);
-        let poractp = (activos * 100)/poblacion;
-        let porconfr = Math.round(porconf * 10000) / 10000;
-        let poractr = Math.round(poract * 10000) / 10000;
-        let porrecr = Math.round(porrec * 10000) / 10000;
-        let pormur = Math.round(pormu * 10000) / 10000;
-        let poractpr = Math.round(poractp * 10000) / 10000;
-        document.getElementById('confirmados').innerHTML = 'Confirmados: ' + confirmados + " (" + porconfr + '% de la poblacion)';
-        document.getElementById('activos').innerHTML = 'Activos: ' + activos + " (" + poractr + '% de los confirmados ' + poractpr + '% de la poblacion)';
-        document.getElementById('recuperados').innerHTML = 'Recuperados: ' + recuperados + " (" + porrecr + '%)';
-        document.getElementById('muertos').innerHTML = 'Muertes: ' + muertos + " (" + pormur + '%)';
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("respuestaC").style.display = "block";
-    }
-    dato.send();
+    //startTime();
 }
 
 function lenguas(){
@@ -113,5 +48,11 @@ function top1(){
 
 function music(){
     document.getElementById("music").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'y') {
+        window.location.href = 'https://www.youtube.com/';
+    }
 }
 // @license-end
